f0a3817774eb370d92d92a33924b5c342c40cd91
MahinSojitra <mahin.sojitra@outlook.com>
2025-02-08 16:34:31 +0530

    fix | errors

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const validateRequest = require("../validations/requestValidator");
+const validateUser = require("../validations/requestValidator");
 const errorHandler = require("../middlewares/errorHandler");
 
 const router = express.Router();
@@ -8,17 +8,17 @@ const router = express.Router();
 // Validate request before processing
 router.post(
   "/register",
-  validateRequest(["email", "password"]),
+  validateUser(["email", "password"]),
   userController.registerUser
 );
 router.post(
   "/apikey",
-  validateRequest(["email", "password"]),
+  validateUser(["email", "password"]),
   userController.getApiKey
 );
 router.post(
   "/regenerate-key",
-  validateRequest(["email", "password"]),
+  validateUser(["email", "password"]),
   userController.regenerateApiKey
 );
 
