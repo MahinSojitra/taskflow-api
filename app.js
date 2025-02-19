5c8de9b4865011d92d5d9c0cb18a81b8b5891d28
MahinSojitra <mahin.sojitra@outlook.com>
2025-02-19 11:33:59 +0530

    fix | fix errors

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
     success: false,
     message: `Route ${req.method} ${req.path} not found`,
     suggestion: similarRoute
-      ? `Did you mean ${similarRoute}?`
+      ? `Did you mean ${similarRoute} ?`
       : "Check documentation at /",
   });
 });
