2d0c63bdef8505db4c0ca36f12922d5ba8887042
MahinSojitra <mahin.sojitra@outlook.com>
2025-02-19 10:47:02 +0530

    fix | fix errors

--- routes/task.js
+++ routes/task.js
@@ -4,6 +4,7 @@ const validateRequest = require("../middlewares/validateRequest");
 const { protect } = require("../middlewares/auth");
 const { taskSchemas } = require("../validations/validationSchemas");
 const { errorHandler } = require("../middlewares/errorHandler");
+const authorize = require("../middlewares/authorize");
 
 const router = express.Router();
 
@@ -11,19 +12,21 @@ const router = express.Router();
 router.use(protect);
 
 // Regular user routes
-router.get("/", taskController.getAllTasks);
+router.get("/", authorize("user", "admin"), taskController.getAllTasks);
 router.post(
   "/",
+  authorize("user", "admin"),
   validateRequest(taskSchemas.create),
   taskController.createTask
 );
-router.get("/:id", taskController.getTask);
+router.get("/:id", authorize("user", "admin"), taskController.getTask);
 router.put(
   "/:id",
+  authorize("user", "admin"),
   validateRequest(taskSchemas.update),
   taskController.updateTask
 );
-router.delete("/:id", taskController.deleteTask);
+router.delete("/:id", authorize("user", "admin"), taskController.deleteTask);
 
 // Admin only route
 router.get("/admin/all", authorize("admin"), taskController.getAllUsersTasks);
